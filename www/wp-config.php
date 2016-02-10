abe8958cf7b053ce5da6a5c9426eb73275f57681
Viktor Prilepin <vicpride@gmail.com>
2016-02-10 16:58:52 +0600

    Victor's commit

--- www/wp-config.php
+++ www/wp-config.php
@@ -14,28 +14,47 @@
  * @package WordPress
  */
 
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define('DB_NAME', 'ugotrumo_bidqadb');
 
-/** MySQL database username */
-define('DB_USER', 'ugotrumo_bidqadb');
+if ( file_exists( dirname( __FILE__ ) . '/wp-config-local.php' ) ) {
+    
+	include( dirname( __FILE__ ) . '/wp-config-local.php' );
+    
+}
+else {
+        // ** MySQL settings - You can get this info from your web host ** //
+        /** The name of the database for WordPress */
+        define('DB_NAME', 'ugotrumo_bidqadb');
 
-/** MySQL database password */
-define('DB_PASSWORD', 'V1ac0m99!');
+        /** MySQL database username */
+        define('DB_USER', 'ugotrumo_bidqadb');
 
-/** MySQL hostname */
-define('DB_HOST', 'localhost');
+        /** MySQL database password */
+        define('DB_PASSWORD', 'V1ac0m99!');
 
-/** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
+        /** MySQL hostname */
+        define('DB_HOST', 'localhost');
 
-/** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
+        /** Database Charset to use in creating database tables. */
+        define('DB_CHARSET', 'utf8');
 
-define('FORCE_SSL_LOGIN', true);
+        /** The Database Collate type. Don't change this if in doubt. */
+        define('DB_COLLATE', '');
 
-define('FORCE_SSL_ADMIN', true);
+        define('FORCE_SSL_LOGIN', true);
+
+        define('FORCE_SSL_ADMIN', true);
+        
+        /**
+        * For developers: WordPress debugging mode.
+        *
+        * Change this to true to enable the display of notices during development.
+        * It is strongly recommended that plugin and theme developers use WP_DEBUG
+        * in their development environments.
+        */
+
+       define('WP_DEBUG', false);
+        
+}
 
 /**#@+
  * Authentication Unique Keys and Salts.
@@ -65,15 +84,7 @@ define('NONCE_SALT',       'put your unique phrase here');
  */
 $table_prefix  = 'wp_';
 
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- */
 
-define('WP_DEBUG', false);
 
 
 /* That's all, stop editing! Happy blogging. */
